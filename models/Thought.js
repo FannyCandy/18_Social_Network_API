cf31f3e1794ecd8f2565417b1f83e49538db97bb
Fanny Tan <fannyc310@gmail.com>
2024-01-16 02:15:00 -0600

    create models

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      require: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema
+  .virtual('reactionCount')
+  .get(function () {
+    return this.reactions.length;
+  })
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
